dc2559021ccd79b3153ca042a1a3da8d6c71ba6f
MirnaMedhat222 <mirna22rona@gmail.com>
2024-12-14 13:26:28 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let g = document.getElementById("g");
+    g.addEventListener("click", click);
+    function click() {
+        alert("thank you");
+    }
+
+
+
+
+
+
+})
